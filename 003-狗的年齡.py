bdf3bc0829058e48c78e84bd9a56c0edc2fc1843
ian1234511 <ban357185@gmail.com>
2025-02-24 19:14:36 +0800

    Update 003-狗的年齡.py

--- 003-狗的年齡.py
+++ 003-狗的年齡.py
@@ -1,11 +1,7 @@
-y = int(input())
-
-if y <= 0:
-    print("Wrong")
-elif y == 1:
-    print(10.5)
-elif y == 2:
-    print(21.0)
+a=int(input())
+if 0<a<=2:
+    print(a*10.5)
+elif a>2:
+    print(21+a*4-2*4)
 else:
-    human_age = 21 + (y - 2) * 4
-    print(human_age)
+    print("Wrong")
