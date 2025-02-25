66a494b9b6a4f08dc6478ac88bf090a8b0e41ee7
ian1234511 <ban357185@gmail.com>
2025-02-25 13:16:01 +0800

    Update 014-三角形-1.py

--- 014-三角形-1.py
+++ 014-三角形-1.py
@@ -1,22 +1,22 @@
-a = int(input())
-b="  "
-for i in range(1, a + 1):
-    for s in range(1, i + 1):
-        print(s, end=" ")
-    print()
-print()
-for i  in range(a,0,-1):
-    for s in range(1, i + 1):
-        print(s, end=" ")
-    print()
-print()
-for i in range(a+1):
-    for m in range(a,i,-1):
-        print(m, end=" ")
+a=int(input())
+for i in range(1,a+1):
+    for s in range(1,i+1):
+        print(s,end=" ")
     print()
 print()
 for i in range(a,0,-1):
-    print(b*(i-1),end="")
-    for s in range(1,a-i+2,1):
+    for s in range(1,i+1):
+        print(s,end=" ")
+    print()
+print()
+for i in range(a+1):
+    for s in range(a,i,-1):
+        print(s,end=" ")
+    print()
+print()
+for i in range(a,0,-1):
+    print("  "*(i-1),end="")
+    for s in range(1, a - i + 2, 1):
         print(s, end=" ")
     print()
+print()
