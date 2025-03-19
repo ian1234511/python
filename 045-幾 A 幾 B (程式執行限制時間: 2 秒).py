ab5b6bd0e64b21d838712e7f85f1464d86fb32f0
ian1234511 <ban357185@gmail.com>
2025-03-19 11:29:51 +0800

    Update 045-幾 A 幾 B (程式執行限制時間: 2 秒).py

--- 045-幾 A 幾 B (程式執行限制時間: 2 秒).py
+++ 045-幾 A 幾 B (程式執行限制時間: 2 秒).py
@@ -1,7 +1,13 @@
-n = int(input().strip())
+n = int(input())
 for _ in range(n):
-    j, k = input().strip().split(", ")
-    A = sum(1 for x, y in zip(j, k) if x == y)
+    s = input()
+    q = s.find(",")
+    j = s[:q]
+    k = s[q + 2:]
+    A = 0
+    for i in range(len(j)):
+        if j[i] == k[i]:
+            A += 1
     B = 0
     for d in set(j):
         B += min(j.count(d), k.count(d))
