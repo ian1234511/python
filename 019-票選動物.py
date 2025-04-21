b2bc773b4f1814f50da46cf8779c5d1b15700ff9
ian1234511 <ban357185@gmail.com>
2025-04-21 11:32:06 +0800

    Update 019-票選動物.py

--- 019-票選動物.py
+++ 019-票選動物.py
@@ -1,4 +1,4 @@
-a = [input().stri() for _ in range(9)]
-t = a.count("Tiger")
-l = a.count("Lion")
-print("Tiger" if t > l else "Lion")
+  a = [input().strip() for _ in range(9)]
+  t = a.count("Tiger")
+  l = a.count("Lion")
+  print("Tiger" if t > l else "Lion")
